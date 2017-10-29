97899146d582a938ebb143dab55969bf97778a7d
nipil <nipil@users.noreply.github.com>
2017-10-29 11:35:09 +0100

    add external sample configuration

---
+++ doc/example-config.py
@@ -0,0 +1,52 @@
+config = {
+    "path": {
+        # do NOT add any leading /
+        "config": "etc/bind",
+        "data": "var/cache/bind",
+    },
+    "secured_permissions": {
+        "root-user": "root",
+        "bind-user": "bind",
+        "bind-group": "bind",
+        "flags": "640",
+    },
+    "master": {
+        "fqdn": "ns1.example.com",
+        "ipv4": "192.0.2.1",
+        "ipv6": "2001:db8:0:1::1",
+    },
+    "slaves": {
+        "ns2.example.com": {
+            "ipv4": "198.51.100.1",
+            "ipv6": "2001:db8:0:2::1",
+        },
+        "ns3.example.com": {
+            "ipv4": "203.0.113.1",
+            "ipv6": "2001:db8:0:3::1",
+        },
+    },
+    "zones": {
+        "example.org": {
+            "dynamic-updates": {
+                "laptop": "A AAAA",
+                "home-server": "ANY",
+            },
+        },
+        "example.com": {
+            "dynamic-updates": {
+                "test": "A",
+            }
+        },
+        "example.net": {},
+        "example.edu": {},
+        "example.biz": {},
+    },
+    "parameters": {
+        "email": "hostmaster@example.com",
+        "ttl": "5m",
+        "refresh": "4h",
+        "retry": "1h",
+        "expire": "1w",
+        "minimum": "3h",
+    },
+}
