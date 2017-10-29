3145ef7bc1b6fc76363f31cc92e316c6fd222748
nipil <nipil@users.noreply.github.com>
2017-10-29 13:18:17 +0100

    load external configuration

--- generate.py
+++ generate.py
@@ -1,60 +1,10 @@
 #!/usr/bin/env python3
 
 # --------------------------------------------------------------------------
-# edit configuration below to suit your needs
-# --------------------------------------------------------------------------
-
-config = {
-    "path": {
-        # do NOT add any leading /
-        "config": "etc/bind",
-        "data": "var/cache/bind",
-    },
-    "secured_permissions": {
-        "root-user": "root",
-        "bind-user": "bind",
-        "bind-group": "bind",
-        "flags": "640",
-    },
-    "master": {
-        "fqdn": "ns1.example.com",
-        "ipv4": "192.0.2.1",
-        "ipv6": "2001:db8:0:1::1",
-    },
-    "slaves": {
-        "ns2.example.com": {
-            "ipv4": "198.51.100.1",
-            "ipv6": "2001:db8:0:2::1",
-        },
-        "ns3.example.com": {
-            "ipv4": "203.0.113.1",
-            "ipv6": "2001:db8:0:3::1",
-        },
-    },
-    "zones": {
-        "example.org": {
-            "dynamic-updates": {
-                "laptop": "A AAAA",
-                "home-server": "ANY",
-            },
-        },
-        "example.com": {}
-    },
-    "parameters": {
-        "email": "hostmaster@example.com",
-        "ttl": "5m",
-        "refresh": "4h",
-        "retry": "1h",
-        "expire": "1w",
-        "minimum": "3h",
-    },
-}
-
-# --------------------------------------------------------------------------
 # edit templates below if you really require it
 # --------------------------------------------------------------------------
 
-templates = {
+all_templates = {
 
 "named.conf": '''
 // This is the primary configuration file for the BIND DNS server named.
@@ -313,9 +263,11 @@ import os.path
 import random
 import re
 import stat
+import sys
 import tarfile
 
-from mako.template import Template
+import importlib
+import mako.template
 
 class Storage:
 
@@ -372,6 +324,44 @@ class Archive:
                 logging.debug("File %s added to tarball" % full_path)
             logging.debug("Tarball %s created" % archive_path)
 
+class Configuration:
+
+    def __init__(self, relative_path):
+        self.load(relative_path)
+
+    def load(self, relative_path):
+        logging.info("Loading configuration file %s" % relative_path)
+        directory_name = os.path.dirname(relative_path)
+        if len(directory_name) > 0:
+            logging.debug("Adding '%s' to system path" % directory_name)
+            sys.path.insert(0, directory_name)
+        file_name = os.path.basename(relative_path)
+        module_name = os.path.splitext(file_name)[0]
+        logging.debug("Loading module %s from file %s located in %s" % (module_name, file_name, directory_name))
+        module = importlib.import_module(module_name)
+        self.config = module.config
+
+    def get_config_path(self):
+        return self.config["path"]["config"]
+
+    def get_data_path(self):
+        return self.config["path"]["data"]
+
+    def get_zones(self):
+        return self.config["zones"]
+
+    def get_master(self):
+        return self.config["master"]
+
+    def get_slaves(self):
+        return self.config["slaves"]
+
+    def get_parameters(self):
+        return self.config["parameters"]
+
+    def get_secured_permissions(self):
+        return self.config["secured_permissions"]
+
 class App:
 
     def __init__(self, storage, overwrite):
@@ -391,77 +381,74 @@ class App:
         d = bytes([r.randint(0, 255) for i in range(length)])
         return base64.b64encode(d).decode("UTF-8")
 
-    def get_template(self, name):
-        return self.templates[name]
-
     def save(self, archive, inner_directory, file_name, content, perms="664"):
         self.archives[archive].store("%s/%s" % (inner_directory, file_name), content, perms, self.overwrite)
 
     def run(self, config, templates):
 
         # named.conf
-        t = Template(templates["named.conf"])
-        r = t.render(CONFIG_DIR=config["path"]["config"])
-        self.save("master-conf", config["path"]["config"], "named.conf", r)
-        self.save("slave-conf", config["path"]["config"], "named.conf", r)
+        t = mako.template.Template(templates["named.conf"])
+        r = t.render(CONFIG_DIR=config.get_config_path())
+        self.save("master-conf", config.get_config_path(), "named.conf", r)
+        self.save("slave-conf", config.get_config_path(), "named.conf", r)
         # named.conf.options
-        t = Template(templates["named.conf.options"])
-        r = t.render(DATA_DIR=config["path"]["data"])
-        self.save("master-conf", config["path"]["config"], "named.conf.options", r)
-        self.save("slave-conf", config["path"]["config"], "named.conf.options", r)
+        t = mako.template.Template(templates["named.conf.options"])
+        r = t.render(DATA_DIR=config.get_data_path())
+        self.save("master-conf", config.get_config_path(), "named.conf.options", r)
+        self.save("slave-conf", config.get_config_path(), "named.conf.options", r)
         # named.conf.options
-        t = Template(templates["key"])
+        t = mako.template.Template(templates["key"])
         r = t.render(
             KEY_NAME="master-slave",
             KEY_ALGO="hmac-sha256",
             KEY_SECRET=self.get_random_base64(32))
-        self.save("master-conf", config["path"]["config"], "auth-master-slave.key", r, "640")
-        self.save("slave-conf", config["path"]["config"], "auth-master-slave.key", r, "640")
+        self.save("master-conf", config.get_config_path(), "auth-master-slave.key", r, "640")
+        self.save("slave-conf", config.get_config_path(), "auth-master-slave.key", r, "640")
         # named.conf.local.master
-        t = Template(templates["named.conf.local.master"])
+        t = mako.template.Template(templates["named.conf.local.master"])
         r = t.render(
-            CONFIG_DIR=config["path"]["config"],
-            DATA_DIR=config["path"]["data"],
-            SLAVES=config["slaves"],
-            ZONES=config["zones"])
-        self.save("master-conf", config["path"]["config"], "named.conf.local", r)
+            CONFIG_DIR=config.get_config_path(),
+            DATA_DIR=config.get_data_path(),
+            SLAVES=config.get_slaves(),
+            ZONES=config.get_zones())
+        self.save("master-conf", config.get_config_path(), "named.conf.local", r)
         # named.conf.local.slave
-        t = Template(templates["named.conf.local.slave"])
+        t = mako.template.Template(templates["named.conf.local.slave"])
         r = t.render(
-            CONFIG_DIR=config["path"]["config"],
-            DATA_DIR=config["path"]["data"],
-            MASTER=config["master"],
-            ZONES=config["zones"])
-        self.save("slave-conf", config["path"]["config"], "named.conf.local", r)
+            CONFIG_DIR=config.get_config_path(),
+            DATA_DIR=config.get_data_path(),
+            MASTER=config.get_master(),
+            ZONES=config.get_zones())
+        self.save("slave-conf", config.get_config_path(), "named.conf.local", r)
 
         # permission script
-        t = Template(templates["secure_permissions.sh"])
+        t = mako.template.Template(templates["secure_permissions.sh"])
         r = t.render(
-            CONFIG_DIR=config["path"]["config"],
-            DATA_DIR=config["path"]["data"],
-            PERMISSIONS=config["secured_permissions"],
-            ZONES=config["zones"])
-        self.save("master-conf", config["path"]["config"], "secure_permissions.sh", r)
-        self.save("slave-conf", config["path"]["config"], "secure_permissions.sh", r)
+            CONFIG_DIR=config.get_config_path(),
+            DATA_DIR=config.get_data_path(),
+            PERMISSIONS=config.get_secured_permissions(),
+            ZONES=config.get_zones())
+        self.save("master-conf", config.get_config_path(), "secure_permissions.sh", r)
+        self.save("slave-conf", config.get_config_path(), "secure_permissions.sh", r)
         # dnssec-key script
-        t = Template(templates["ensure_dnssec_keys.sh"])
+        t = mako.template.Template(templates["ensure_dnssec_keys.sh"])
         r = t.render(
-            CONFIG_DIR=config["path"]["config"],
-            PERMISSIONS=config["secured_permissions"],
-            ZONES=config["zones"])
-        self.save("master-conf", config["path"]["config"], "ensure_dnssec_keys.sh", r)
+            CONFIG_DIR=config.get_config_path(),
+            PERMISSIONS=config.get_secured_permissions(),
+            ZONES=config.get_zones())
+        self.save("master-conf", config.get_config_path(), "ensure_dnssec_keys.sh", r)
 
         # per zone stuff
-        for zone_name, zone_data in config["zones"].items():
+        for zone_name, zone_data in config.get_zones().items():
 
             # zone file
-            t = Template(templates["zone_file"])
+            t = mako.template.Template(templates["zone_file"])
             r = t.render(
-                PARAMETERS=config["parameters"],
-                MASTER=config["master"],
-                SLAVES=config["slaves"],
+                PARAMETERS=config.get_parameters(),
+                MASTER=config.get_master(),
+                SLAVES=config.get_slaves(),
                 ZONE_NAME=zone_name)
-            self.save("master-zones", config["path"]["data"], "db.%s" % zone_name, r)
+            self.save("master-zones", config.get_data_path(), "db.%s" % zone_name, r)
 
             # nsupdate keys
             try:
@@ -469,22 +456,23 @@ class App:
             except KeyError:
                 nsupdate = {}
             for rr_name in nsupdate:
-                t = Template(templates["key"])
+                t = mako.template.Template(templates["key"])
                 r = t.render(
                     KEY_NAME="%s.%s" % (rr_name, zone_name),
                     KEY_ALGO="hmac-sha256",
                     KEY_SECRET=self.get_random_base64(32))
                 f = "nsupdate-keys/%s/%s.%s.key" % (zone_name, rr_name, zone_name)
-                self.save("master-conf", config["path"]["config"], f, r, "640")
+                self.save("master-conf", config.get_config_path(), f, r, "640")
 
         # generate install script
-        t = Template(templates["install.sh"])
-        r = t.render(MASTER=config["master"], SLAVES=config["slaves"])
+        t = mako.template.Template(templates["install.sh"])
+        r = t.render(MASTER=config.get_master(), SLAVES=config.get_slaves())
         self.storage.write_file("install.sh", r, self.overwrite)
 
-if __name__ == '__main__':
+def main():
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config-file", metavar="CFG", default="config.py")
     parser.add_argument("-d", "--destination", metavar="DEST", default="build")
     parser.add_argument("-f", "--force", action="store_true")
     parser.add_argument("-l", "--log-level", metavar="LVL", choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"], default="WARNING")
@@ -499,4 +487,12 @@ if __name__ == '__main__':
 
     storage = Storage(args.destination)
     app = App(storage, args.force)
-    app.run(config, templates)
+
+    cfg = Configuration(args.config_file)
+    app.run(cfg, all_templates)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        logging.error(e)
